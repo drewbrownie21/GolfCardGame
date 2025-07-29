c6e5d6a28c222c61f50b4cee8bd491a0c0e5ffb4
drewbrownie21 <36270973+drewbrownie21@users.noreply.github.com>
2025-07-29 00:15:19 -0700

    prettier

--- src/factory/generateDeck.ts
+++ src/factory/generateDeck.ts
@@ -2,7 +2,7 @@ export type Card = {
   suit: string;
   rank: number;
   value: number;
-  flipped: boolean
+  flipped: boolean;
 };
 
 export function GenerateDeck() {
@@ -26,7 +26,12 @@ export function GenerateDeck() {
     let ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
     let suits = ["C", "D", "H", "S"];
     let newDeck: Card[] = ranks.flatMap((rank) =>
-      suits.map((suit) => ({ suit, rank, value: getValue(rank), flipped: false })),
+      suits.map((suit) => ({
+        suit,
+        rank,
+        value: getValue(rank),
+        flipped: false,
+      })),
     );
 
     return randomizeDeck(newDeck);
