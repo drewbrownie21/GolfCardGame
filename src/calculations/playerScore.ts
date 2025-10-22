0477915282289d7fa26246cd5d880fb6d0edbddc
drewbrownie21 <36270973+drewbrownie21@users.noreply.github.com>
2025-10-21 21:03:28 -0700

    Update playerScore.ts

--- src/calculations/playerScore.ts
+++ src/calculations/playerScore.ts
@@ -2,13 +2,21 @@ import { type Hand2D } from "../components/Game/Game/Game";
 
 export function playerScore(hand: Hand2D) {
   let score: number = 0;
-  let scoreArray: number[] = [];
 
-  for (let i = 0; i < hand.length; i++) {
-    for (let j = 0; j < hand[i].length; j++) {
-      scoreArray = [...scoreArray, hand[i][j]?.value];
-    }
+  let total = 0;
+
+  console.log(hand)
+
+  for (let c = 0; c < hand[0].length; c++) {
+    const top = hand[0][c];
+    const bottom = hand[1][c];
+
+    if (top === bottom) continue; // matching column = 0 points
+
+    total += top + bottom;
   }
-  console.log("Score: " + score);
+
+  console.log(total)
+
   return score;
 }
